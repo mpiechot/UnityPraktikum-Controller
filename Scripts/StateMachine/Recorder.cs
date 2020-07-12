af37e87261f7230ef871f0534c8028dd48797ed4
Grapehippo <haraldkangschroeder@gmail.com>
2020-07-12 13:25:50 +0200

    -

---
+++ Scripts/StateMachine/Recorder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recorder : MonoBehaviour
+{
+
+    public bool record_position = false;
+    public bool record_rotation = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(record_position){
+            //record position
+        }
+        if(record_rotation){
+            //record rotation
+        }
+    }
+}
